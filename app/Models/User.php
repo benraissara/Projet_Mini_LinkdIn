4f4fa39ccd8c7d916e342162f454f36e0ac58cb6
Sara ben rais <benrais.sara@ensam-casa.ma>
2026-04-16 15:30:14 +0100

    partie auth de sara

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,9 +7,27 @@ use Database\Factories\UserFactory;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use PHPOpenSourceSaver\JWTAuth\Contracts\JWTSubject;
 
-class User extends Authenticatable
+class User extends Authenticatable implements JWTSubject
 {
+
+    public function getJWTIdentifier()
+    {
+        return $this->getKey();
+    }
+    public function getJWTCustomClaims(){
+        return [];
+    }
+
+
+
+
+
+
+
+
+
     /** @use HasFactory<UserFactory> */
     use HasFactory, Notifiable;
 
